55506b66144e8eafb4e662f7d858ec47797fab2f
Narasimha <arroju.narasimha@lntinfotech.com>
2021-10-14 16:57:32 +0530

    added Spring boot and Angular

---
+++ OnlineExamBackend/src/main/java/com/lti/pg/g8/onlineexambackend/OnlineExamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lti.pg.g8.onlineexambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamBackendApplication.class, args);
+    }
+
+}
---
+++ OnlineExamBackend/src/test/java/com/lti/pg/g8/onlineexambackend/OnlineExamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.pg.g8.onlineexambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
