1017d03c4ac31939c52cf697a1bb402d715fc1d3
Chaithali <Chaithali.Suvarna@lntinfotech.com>
2021-10-18 09:50:13 +0530

    Added admin login functionality

--- OnlineExamBackend/src/main/java/com/lti/pg/g8/onlineexambackend/controller/AdminController.java
+++ OnlineExamBackend/src/main/java/com/lti/pg/g8/onlineexambackend/controller/AdminController.java
@@ -1,42 +1,59 @@
 package com.lti.pg.g8.onlineexambackend.controller;
 
-import com.lti.pg.g8.onlineexambackend.model.Exam;
-import com.lti.pg.g8.onlineexambackend.service.ExamService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.pg.g8.onlineexambackend.model.Admin;
+import com.lti.pg.g8.onlineexambackend.model.Exam;
+import com.lti.pg.g8.onlineexambackend.service.AdminLoginService;
+import com.lti.pg.g8.onlineexambackend.service.ExamService;
+
 @RestController
-@RequestMapping("/admin/exams")
+@RequestMapping("/admin")
 public class AdminController {
 
-    final
+    @Autowired
     ExamService examService;
-
-    public AdminController(ExamService examService) {
-        this.examService = examService;
+    
+    @Autowired
+    AdminLoginService adminLoginService;
+    
+    @PostMapping("/login")
+    public ResponseEntity<Boolean> validateAdmin(@RequestBody Admin admin){
+    	return new ResponseEntity<>(this.adminLoginService.validateAdmin(admin),HttpStatus.OK);
     }
+    
 
-    @GetMapping("")
+    @GetMapping("/exams")
     public ResponseEntity<List<Exam>> getAllExams(){
         return new ResponseEntity<>(this.examService.getAllExams(), HttpStatus.OK);
     }
     
-    @GetMapping("/{examId}")
+    @GetMapping("/exams/{examId}")
     public ResponseEntity<Exam> getExamById(@PathVariable Long examId){
         return new ResponseEntity<>(this.examService.getExamById(examId), HttpStatus.OK);
     }
 
-    @PostMapping("")
+    @PostMapping("/exams")
     public ResponseEntity<Exam> addNewExam(@RequestBody Exam exam){
         return new ResponseEntity<>(this.examService.addOrUpdate(exam), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/{examId}")
+    @DeleteMapping("/exams/{examId}")
     public ResponseEntity<Boolean> deleteExam(@PathVariable Long examId){
         return new ResponseEntity<>(this.examService.deleteExamById(examId), HttpStatus.OK);
     }
 
+    
+    
 }
